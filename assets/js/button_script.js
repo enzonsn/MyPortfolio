13a10356676dcfba211fde27922e08e650e852cb
enzonsn <77365483+enzonsn@users.noreply.github.com>
2025-08-05 15:57:46 -0500

    surely we got it this time

--- assets/js/button_script.js
+++ assets/js/button_script.js
@@ -1,17 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
     const syncPointer = ({ x: pointerX, y: pointerY }) => {
-    const x = pointerX.toFixed(2);
-    const y = pointerY.toFixed(2);
-    const xp = (pointerX / window.innerWidth).toFixed(2);
-    const yp = (pointerY / window.innerHeight).toFixed(2);
-    document.documentElement.style.setProperty('--x', x);
-    document.documentElement.style.setProperty('--xp', xp);
-    document.documentElement.style.setProperty('--y', y);
-    document.documentElement.style.setProperty('--yp', yp);
+        const x = pointerX.toFixed(2);
+        const y = pointerY.toFixed(2);
+        const xp = (pointerX / window.innerWidth).toFixed(2);
+        const yp = (pointerY / window.innerHeight).toFixed(2);
+        document.documentElement.style.setProperty('--x', x);
+        document.documentElement.style.setProperty('--xp', xp);
+        document.documentElement.style.setProperty('--y', y);
+        document.documentElement.style.setProperty('--yp', yp);
+    };
+
+    document.body.addEventListener('pointermove', syncPointer);
 
     const soloButton = document.getElementById('solo-button');
-    soloButton.addEventListener('click', (event) => {
-        window.location.href = '../../form.html';
-    });
-};
-document.body.addEventListener('pointermove', syncPointer);
+    if (soloButton) {
+        soloButton.addEventListener('click', () => {
+            window.location.href = '../../form.html';
+        });
+    } else {
+        console.error('solo-button not found');
+    }
+});
