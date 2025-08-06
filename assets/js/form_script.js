7a4a3a7b48ac5aa256db2ef1e00fdcebc04efb35
enzonsn <77365483+enzonsn@users.noreply.github.com>
2025-08-05 22:41:06 -0500

    added js to form page and autofilled company value to the email value

---
+++ assets/js/form_script.js
@@ -0,0 +1,14 @@
+let emailInput;
+window.addEventListener('DOMContentLoaded', () => {
+    const emailField = document.getElementById('email');
+    const companyField = document.getElementById('company-name');
+
+    emailField.addEventListener('change', function () {
+        emailInput = this.value;
+        emailToCompany();
+    });
+
+    function emailToCompany() {
+        companyField.value = emailInput;
+    }
+});
